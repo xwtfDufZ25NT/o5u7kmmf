4e1fc9c100dcc9c88d7349a37f35717775baa975
xwtfDufZ25NT <xwtfDufZ25NT@mail.dwinqc.eu.org>
2020-09-11 23:55:08 +0800

    First commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SH_PATH=$(cd "$(dirname "$0")";pwd)
+cd ${SH_PATH}
+
+install(){
+    echo "进行安装。。。"
+    cd ${SH_PATH}/i3oHPqjqAQbx/install
+    ibmcloud target --cf
+    ibmcloud cf install
+    ibmcloud cf push
+    echo "安装完成。"
+}
+
+install
+exit 0
+
---
+++ install/bin/runtime.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd bin
+rm -rf .cache
+mkdir .cache
+cd .cache
+wget https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip v2ray-linux-64.zip
+cp ./v2ray ../run
+cp ./v2ctl ../run-ctl
+cd ..
+
+chmod +x *
+./run -c config.json
