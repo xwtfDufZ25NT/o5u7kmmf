a5796168a4bac1211a3e25c379b144215337754e
xwtfDufZ25NT <xwtfDufZ25NT@mail.dwinqc.eu.org>
2020-09-12 00:31:34 +0800

    Fix runtime script

--- install/bin/runtime.sh
+++ install/bin/runtime.sh
@@ -7,7 +7,7 @@ cd .cache
 wget https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip v2ray-linux-64.zip
 cp ./v2ray ../run
-cp ./v2ctl ../run-ctl
+cp ./v2ctl ../
 cd ..
 
 chmod +x *
